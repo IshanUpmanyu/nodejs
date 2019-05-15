0aeb7724ad4b9ba4daf8cc7569cacd8e94fcb7c6
Ishan <ishan.upmanyu@gmail.com>
2019-05-15 17:14:55 +0530

    Added express introduction

--- express-tutorial/index.js
+++ express-tutorial/index.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send(['html', 'css', 'javascript', 'java']);
+});
+
 app.listen(3000, () => console.log('Listening of port 3000.'));
